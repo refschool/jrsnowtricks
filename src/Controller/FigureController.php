f9be029d71d82020a696790728cc94db3c661526
Nayte91 <nayte91@gmail.com>
2020-02-26 12:02:43 +0100

    D conf (.env.local) & first migration

---
+++ src/Controller/FigureController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Figure;
+use App\Form\FigureType;
+use App\Repository\FigureRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/figure")
+ */
+class FigureController extends AbstractController
+{
+    /**
+     * @Route("/", name="figure_index", methods={"GET"})
+     */
+    public function index(FigureRepository $figureRepository): Response
+    {
+        return $this->render('figure/index.html.twig', [
+            'figures' => $figureRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="figure_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $figure = new Figure();
+        $form = $this->createForm(FigureType::class, $figure);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($figure);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('figure_index');
+        }
+
+        return $this->render('figure/new.html.twig', [
+            'figure' => $figure,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="figure_show", methods={"GET"})
+     */
+    public function show(Figure $figure): Response
+    {
+        return $this->render('figure/show.html.twig', [
+            'figure' => $figure,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="figure_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Figure $figure): Response
+    {
+        $form = $this->createForm(FigureType::class, $figure);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('figure_index');
+        }
+
+        return $this->render('figure/edit.html.twig', [
+            'figure' => $figure,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="figure_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Figure $figure): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$figure->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($figure);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('figure_index');
+    }
+}
