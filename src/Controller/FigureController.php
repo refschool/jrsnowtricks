f5214a0a3cbc9f3ad7f1d69f5f308bcc78e0fd18
Nayte <nayte91@gmail.com>
2020-06-18 16:48:38 +0200

    FRONT : Added arrow to scroll down to the tricks, on index

--- src/Controller/FigureController.php
+++ src/Controller/FigureController.php
@@ -11,9 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/")
- */
+/** @Route("/") */
 class FigureController extends AbstractController
 {
     /**
